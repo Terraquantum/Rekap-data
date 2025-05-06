f69656068abf9b1e79fc29ebe3b07817c8ac23db
Raihan Alyfan Sardi <90737998+Terraquantum@users.noreply.github.com>
2025-05-06 19:32:02 +0700

    NegroAdli

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -22,4 +22,57 @@ class Welcome extends CI_Controller {
 	{
 		$this->load->view('welcome_message');
 	}
+
+
+	public function create($id=false){
+		$this->load->helper('form');
+		$this->load->library('form_validation');
+
+		$this->form_validation->set_rules('name', 'Name', 'required|max_length[30]');
+		$this->form_validation->set_rules('description', 'Description', 'required');
+
+		if($this->form_validation->run() == FALSE){
+			$this->load->view('header');
+			$this->load->view('create');
+			$this->load->view('footer');
+		} else {
+			$id = uniqid('item', TRUE);
+
+			$config['upload_path'] = './upload/post';
+			$config['allowed_types'] = 'jpg|jpeg|png';
+			$config['max_size'] = '100000';
+			$config['file_ext_tolower'] = TRUE;
+			$config['file_name'] = str_replace('.','_',$id);
+
+			$this->load->library('upload', $config);
+
+			if(! $this->upload->do_upload('image1')){
+				$this->session->set_flashdata('error', $this->upload->display_errors());
+				redirect('welcome/index');
+			} else {
+				$filename = $this->upload->data('file_name');
+				$this->model->create($id, $filename);
+				redirect('');
+			}
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
